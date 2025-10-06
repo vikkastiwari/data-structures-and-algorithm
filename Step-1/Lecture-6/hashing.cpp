234ba29d6df405b90a01222c109bf248c1245599
Vikas Tiwari <vikas.tiwari@newfold.com>
2025-10-06 23:58:31 +0530

    occurrences of each element

--- Step-1/Lecture-6/hashing.cpp
+++ Step-1/Lecture-6/hashing.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <unordered_map>
 using namespace std;
 
-int main() {
-     cout << 'B' << endl;
+// Given an array, we have found the number of occurrences of each element in the array.
+
+int main()
+{
+     int arr[] = {10, 5, 10, 15, 10, 5};
+     int size = sizeof(arr)/sizeof(arr[0]);
+     unordered_map<int, int> hash;
+
+     for(int i=0;i<size;i++) {
+          hash[arr[i]]++;
+     }
+
+     for(auto data : hash) {
+          cout << data.first << " " << data.second << endl;
+     } 
+
      return 0;
 }
